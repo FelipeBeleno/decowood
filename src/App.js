dfb68b7f10d878ddfcea8e3b0f1b28f6ed12327b
FelipeBeleno <felipebelenoo1101@gmial.com>
2022-10-07 15:13:24 -0500

    three commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Draggable from 'react-draggable';
 import { Fragment, useCallback, useState } from 'react';
 import downloadjs from 'downloadjs';
 import html2canvas from 'html2canvas';
+import { Button } from '@mui/material';
 
 
 function App() {
@@ -11,7 +12,6 @@ function App() {
 
   const [position, setPosition] = useState({ x: 0, y: 0 });
   const [image, setImage] = useState("");
-
   const trackPos = (data) => {
     console.log(position)
     setPosition({ x: data.x, y: data.y });
@@ -24,10 +24,6 @@ function App() {
     setImage(imgUrl)
   }
 
-
-
-
-
   const hanldeClickImage = useCallback(async () => {
     const canvas = await html2canvas(document.getElementById('image-id'));
     const dataURL = canvas.toDataURL('image/png');
@@ -36,10 +32,25 @@ function App() {
 
   return (
     <Fragment>
-      <input type='file' accept='image/*' onChange={handleImage} capture="environment" />
-      <button onClick={() => {
-        hanldeClickImage()
-      }}>Descargar</button>
+
+      <br />
+      <br />
+      <div className='conatiner__menu'>
+
+        <Button variant="contained">
+          <input className='inputClass' type='file' accept='image/*' onChange={handleImage} capture="environment" />
+        </Button>
+        <br />
+
+        {
+          image !== ""
+          &&
+          <Button variant="contained" onClick={() => {
+            hanldeClickImage()
+          }}>Descargar</Button>
+        }
+      </div>
+
       {/*
         se activara cuando se cargue la imagen de la camara 
         limitar la posicion a 0 , 0
@@ -47,22 +58,29 @@ function App() {
       <div className='content' >
 
         <div className="App" id="image-id" style={{ backgroundImage: `url("${image}")`, backgroundSize: '100% 100%', backgroundRepeat: 'no-repeat' }}>
-          <Draggable
-            //axis="x"
-            //handle=".handle"
-            defaultPosition={{ x: 0, y: 0 }}
-            onDrag={(e, data) => trackPos(data)}
-            onStart={(e, data) => {
-              console.log(e, data)
-            }}
 
-            onStop={(e, data) => {
-              console.log(e, data)
-            }}
-          >
+          {
+            image === ""
+              ? <h1 className=''>Sube una imagen horizontal</h1>
+              : <Draggable
+                //axis="x"
+                //handle=".handle"
+                defaultPosition={{ x: 0, y: 0 }}
+                onDrag={(e, data) => trackPos(data)}
+                onStart={(e, data) => {
+                  console.log(e, data)
+                }}
 
-            <img src='/example3.png' alt='' style={{ width: 220, height: 220 }} />
-          </Draggable>
+                onStop={(e, data) => {
+                  console.log(e, data)
+                }}
+              >
+
+                <img src='/example3.png' alt='' style={{ width: 220, height: 220 }} />
+              </Draggable>
+          }
+
+
 
         </div>
       </div>
